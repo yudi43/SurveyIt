f8b638cafc47e4fd03bc65c47be4fe6eb5d97ddb
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-15 08:30:51 +0530

    changed greeting again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
+  res.send({ bye: "buchadddy" });
 });
 
 const PORT = process.env.PORT || 5000;
